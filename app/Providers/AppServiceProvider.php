45bb3ac105b20a53cbfc7adc4fd7b6d12b7e6390
jlduran43 <jldurancastro@gmail.com>
2026-09-23 10:20:16 -0300

    se cambia a español mensaje correo verificacion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, string $url) {
+
+            return (new MailMessage)
+                ->subject('Verifica tu correo electrónico - Tickets PRZ')
+                ->greeting('¡Hola ' . $notifiable->name . '!')
+                ->line('Gracias por registrarte en Tickets PRZ.')
+                ->line('Para activar tu cuenta y comenzar a comprar tus tickets, necesitamos verificar tu correo electrónico.')
+                ->action('Verificar correo electrónico', $url)
+                ->line('Este enlace de verificación tiene una duración limitada por seguridad.')
+                ->line('Si tú no creaste esta cuenta, puedes ignorar este mensaje.')
+                ->salutation('Saludos, Parque Museo Pedro del Río Zañartu');
+        });
     }
 }
