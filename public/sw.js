3eba78191f8b273fa5929911acd755daf43c492e
jlduran43 <jldurancastro@gmail.com>
2026-09-24 14:17:01 -0300

    se modiica worker server

--- public/sw.js
+++ public/sw.js
@@ -1,103 +1,72 @@
-const CACHE_NAME =
-    'prz-scanner-v1';
-
+const CACHE_NAME = 'prz-scanner-v2';
 
 const OFFLINE_FILES = [
-
+    '/control',
     '/offline/tickets_public.pem',
-
     '/js/ticket-offline.js',
-
     '/vendor/html5-qrcode/html5-qrcode.min.js'
-
 ];
 
+self.addEventListener('install', event => {
 
-self.addEventListener(
-    'install',
-    event => {
-
-        event.waitUntil(
-            caches
-                .open(
-                    CACHE_NAME
-                )
-                .then(
-                    cache =>
-                        cache.addAll(
-                            OFFLINE_FILES
-                        )
-                )
-        );
-
-        self.skipWaiting();
-    }
-);
-
-
-self.addEventListener(
-    'activate',
-    event => {
-
-        event.waitUntil(
-            self.clients.claim()
-        );
-
-    }
-);
-
-
-self.addEventListener(
-    'fetch',
-    event => {
-
-        if (
-            event.request.method
-            !== 'GET'
-        ) {
-            return;
-        }
-
-
-        event.respondWith(
-
-            fetch(
-                event.request
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache =>
+                cache.addAll(OFFLINE_FILES)
             )
-                .then(
-                    response => {
+    );
 
-                        const clone =
-                            response.clone();
+    self.skipWaiting();
+});
 
+self.addEventListener('activate', event => {
 
-                        caches
-                            .open(
-                                CACHE_NAME
-                            )
-                            .then(
-                                cache => {
+    event.waitUntil(
+        Promise.all([
+            self.clients.claim(),
 
-                                    cache.put(
-                                        event.request,
-                                        clone
-                                    );
+            caches.keys().then(names => {
+                return Promise.all(
+                    names.map(name => {
+                        if (name !== CACHE_NAME) {
+                            return caches.delete(name);
+                        }
+                    })
+                );
+            })
+        ])
+    );
+});
 
-                                }
-                            );
+self.addEventListener('fetch', event => {
 
-
-                        return response;
-                    }
-                )
-
-                .catch(
-                    () =>
-                        caches.match(
-                            event.request
-                        )
-                )
-
-        );
+    if (event.request.method !== 'GET') {
+        return;
     }
-);
+
+    event.respondWith(
+
+        fetch(event.request)
+            .then(response => {
+
+                const clone = response.clone();
+
+                caches
+                    .open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(
+                            event.request,
+                            clone
+                        );
+                    });
+
+                return response;
+            })
+
+            .catch(() =>
+                caches.match(event.request)
+            )
+
+    );
+});
